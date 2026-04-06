a618cfd826153d468023d06c57573647c8955e30
ejoneill <ejosephoneill@gmail.com>
2026-04-06 14:02:05 -0600

    created a user controller, user routes, and updated the seed

--- server/server.js
+++ server/server.js
@@ -1,15 +1,20 @@
-const db = require('./db/db');
+const express = require("express");
+const cors = require("cors");
 
-const express = require('express');
-const cors = require('cors');
 const app = express();
 const port = 8080;
+
+const userRoutes = require("./routes/users");
 
 app.use(express.json());
 app.use(cors());
 
-app.get('/', (req,res) => {
-  return res.status(200).json({message: 'Welcome to the empty route traveler ♡(˶>⩊<˶)'})
+app.use("/users", userRoutes);
+
+app.get("/", (req, res) => {
+  return res
+    .status(200)
+    .json({ message: "Welcome to the empty route traveler ♡(˶>⩊<˶)" });
 });
 
 app.listen(port, () => console.log(`Express server listening on port ${port}`));
