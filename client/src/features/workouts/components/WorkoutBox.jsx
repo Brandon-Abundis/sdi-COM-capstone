83eb294673921a50e580766bad109ce71c64a718
Tafari <tafarigriffith2016@gmail.com>
2026-04-07 14:54:50 -0600

    Adds edit functionality to Modal; Makes it not blue

--- client/src/features/workouts/components/WorkoutBox.jsx
+++ client/src/features/workouts/components/WorkoutBox.jsx
@@ -2,10 +2,11 @@ import "../styles/WorkoutBox.css";
 // importing because tailwind was getting too long D:
 import Icon from "./Icon";
 
-export default function WorkoutBox({info}) {
-  let {title, type} = info
+export default function WorkoutBox({ details, onClick }) {
+  let { title, type } = details;
   return (
     <div
+      onClick={onClick}
       id={"WorkoutBox"}
       className={
         "border-2 border-[#240014] hover:border-[#ee80ff] hover:scale-105 cursor-pointer active:scale-100"
@@ -23,9 +24,7 @@ export default function WorkoutBox({info}) {
         >
           {title}
         </h2>
-        <Icon
-          type={type}
-        />
+        <Icon type={type} />
       </div>
       <div className={"m-4 w-60 h-49 bg-[#220d23]"}>
         <h2 className={"text-center mt-1"}> this is test text</h2>
