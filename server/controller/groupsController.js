cea1d96956102b6200a3f65f7665c528c308f2a0
ejoneill <ejosephoneill@gmail.com>
2026-04-07 10:29:09 -0600

    added endpoint instructions into README

--- server/controller/groupsController.js
+++ server/controller/groupsController.js
@@ -19,4 +19,52 @@ const getById = async (req, res) => {
   }
 };
 
-module.exports = { getAll, getById };
+const getGoalsById = async (req, res) => {
+  const { id } = req.params;
+  try {
+    //uncomment these code when user_goals is ready
+    // const result = await db("group_goals").select("*").where("group_id", id).first();
+    // res.status(200).send(result);
+    res
+      .status(200)
+      .send({ message: "you made it, the data you require is not ready yet" });
+  } catch (err) {
+    res.status(500).send({ message: err });
+  }
+};
+
+const getWorkoutsById = async (req, res) => {
+  const { id } = req.params;
+  try {
+    //uncomment these code when user_goals is ready
+    // const result = await db("group_workouts").select("*").where("group_id", id).first();
+    // res.status(200).send(result);
+    res
+      .status(200)
+      .send({ message: "you made it, the data you require is not ready yet" });
+  } catch (err) {
+    res.status(500).send({ message: err });
+  }
+};
+
+const getEventsById = async (req, res) => {
+  const { id } = req.params;
+  try {
+    //uncomment these code when user_goals is ready
+    // const result = await db("group_events").select("*").where("group_id", id).first();
+    // res.status(200).send(result);
+    res
+      .status(200)
+      .send({ message: "you made it, the data you require is not ready yet" });
+  } catch (err) {
+    res.status(500).send({ message: err });
+  }
+};
+
+module.exports = {
+  getAll,
+  getById,
+  getGoalsById,
+  getWorkoutsById,
+  getEventsById,
+};
