c782392501cd1eeaa2ed0d3eb6300e39500cd6d6
Julian Panelo <jpemail124@gmail.com>
2026-04-07 13:46:28 -0700

    added month changes functionality, planning on adding CalendarPopulation mechanic

---
+++ client/src/features/calendar/programs/CalendarPopulator.jsx
@@ -0,0 +1,6 @@
+// this will populate into each calendar day, if data is available.
+//types of workouts:
+// Group Workouts: -Workouts fetched from group
+// Personal Workouts: -Workouts personally tracking
+//Rival Workout: -Workout you and rival competing on (have questions on mechanic, will rival and you have to match eachother? if so will rival be public or priv? if not then does that mean rival workout is optional?)
+//Special Workout Events: -Workout events app-wide(?) (e.g Pushups x2 XP or other)
