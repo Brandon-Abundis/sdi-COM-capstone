c2058f16d6153543cd05256914359904658f64c1
ejoneill <ejosephoneill@gmail.com>
2026-04-07 09:53:38 -0600

    created all user routes/controller no data ready yet

--- server/routes/users.js
+++ server/routes/users.js
@@ -3,11 +3,19 @@ const {
   getAll,
   getById,
   getGroupsById,
+  getGoalsById,
+  getWorkoutsById,
+  getEventsById,
+  getScoresById,
 } = require("../controller/userController");
 const router = express();
 
 router.get("/", getAll);
 router.get("/id/:id", getById);
 router.get("/groups/id/:id", getGroupsById);
+router.get("/user_goals/id/:id", getGoalsById);
+router.get("/user_workouts/id/:id", getWorkoutsById);
+router.get("/user_events/id/:id", getEventsById);
+router.get("/scores/id/:id", getScoresById);
 
 module.exports = router;
