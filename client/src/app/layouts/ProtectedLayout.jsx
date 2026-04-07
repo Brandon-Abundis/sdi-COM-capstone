537dc6c9b049260dac2325a8b1f55bb6e61edabb
kobe-everson <everson.kobe@gmail.com>
2026-04-07 12:34:52 -0700

    updated with chat and groups and adjusted navbar

--- client/src/app/layouts/ProtectedLayout.jsx
+++ client/src/app/layouts/ProtectedLayout.jsx
@@ -1,9 +1,10 @@
 import { Outlet } from "react-router-dom";
+import Navbar from "../Navbar/Navbar.jsx";
 
 export default function ProtectedLayout() {
   return (
     <div className="min-h-screen bg-[#0f0d17]">
-      {/* Navbar inserted here when ready */}
+      <Navbar />
       <Outlet />
     </div>
   );
