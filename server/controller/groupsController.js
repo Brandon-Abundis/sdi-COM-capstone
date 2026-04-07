46262c7657b2482aa0c433c38bffe4948e95f48a
ejoneill <ejosephoneill@gmail.com>
2026-04-07 14:17:10 -0600

    uncommented the endpoints with new seed

--- server/controller/groupsController.js
+++ server/controller/groupsController.js
@@ -55,13 +55,28 @@ const getById = async (req, res) => {
 const getGoalsById = async (req, res) => {
   const { id } = req.params;
   try {
-    //uncomment these code when user_goals is ready
-    // const result = await db("group_goals").select("*").where("group_id", id).first();
-    // res.status(200).send(result);
-    res
-      .status(200)
-      .send({ message: "you made it, the data you require is not ready yet" });
+    const result = await db("group_goals")
+      .select("*")
+      .where("group_id", id)
+      .first();
+    if (result) {
+      createLog({
+        method: "GET",
+        action: "FETCH_GROUP_GOALS",
+        status_code: 200,
+        user_id: id,
+        metadata: JSON.stringify(result),
+      });
+    }
+    res.status(200).send(result);
   } catch (err) {
+    createLog({
+      method: "GET",
+      action: "FETCH_GROUPS_GOALS",
+      status_code: 500,
+      user_id: id,
+      metadata: err,
+    });
     res.status(500).send({ message: err });
   }
 };
@@ -69,13 +84,28 @@ const getGoalsById = async (req, res) => {
 const getWorkoutsById = async (req, res) => {
   const { id } = req.params;
   try {
-    //uncomment these code when user_goals is ready
-    // const result = await db("group_workouts").select("*").where("group_id", id).first();
-    // res.status(200).send(result);
-    res
-      .status(200)
-      .send({ message: "you made it, the data you require is not ready yet" });
+    const result = await db("group_workouts")
+      .select("*")
+      .where("group_id", id)
+      .first();
+    if (result) {
+      createLog({
+        method: "GET",
+        action: "FETCH_GROUP_WORKOUTS",
+        status_code: 200,
+        user_id: id,
+        metadata: JSON.stringify(result),
+      });
+    }
+    res.status(200).send(result);
   } catch (err) {
+    createLog({
+      method: "GET",
+      action: "FETCH_GROUP_WORKOUTS",
+      status_code: 500,
+      user_id: id,
+      metadata: err,
+    });
     res.status(500).send({ message: err });
   }
 };
@@ -83,13 +113,28 @@ const getWorkoutsById = async (req, res) => {
 const getEventsById = async (req, res) => {
   const { id } = req.params;
   try {
-    //uncomment these code when user_goals is ready
-    // const result = await db("group_events").select("*").where("group_id", id).first();
-    // res.status(200).send(result);
-    res
-      .status(200)
-      .send({ message: "you made it, the data you require is not ready yet" });
+    const result = await db("group_events")
+      .select("*")
+      .where("group_id", id)
+      .first();
+    if (result) {
+      createLog({
+        method: "GET",
+        action: "FETCH_GROUP_EVENTS",
+        status_code: 200,
+        user_id: id,
+        metadata: JSON.stringify(result),
+      });
+    }
+    res.status(200).send(result);
   } catch (err) {
+    createLog({
+      method: "GET",
+      action: "FETCH_GROUP_EVENTS",
+      status_code: 500,
+      user_id: id,
+      metadata: err,
+    });
     res.status(500).send({ message: err });
   }
 };
