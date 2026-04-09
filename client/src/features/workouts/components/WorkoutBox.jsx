bfcb8115186c5226ae5eddd814e7ff77d62c10bc
Tafari <tafarigriffith2016@gmail.com>
2026-04-09 14:23:23 -0600

    Styles workout page to match modal and rest of site

--- client/src/features/workouts/components/WorkoutBox.jsx
+++ client/src/features/workouts/components/WorkoutBox.jsx
@@ -12,7 +12,7 @@ export default function WorkoutBox({ details, onClick }) {
       onClick={onClick}
       id={"WorkoutBox"}
       className={
-        "border-2 border-[#240014] hover:border-[#ee80ff] hover:scale-105 cursor-pointer active:scale-100"
+        "border-2 border-[#240014] hover:border-[#ee80ff] hover:scale-105 cursor-pointer active:scale-90"
       }
     >
       <div
@@ -22,17 +22,17 @@ export default function WorkoutBox({ details, onClick }) {
       >
         <h2
           className={
-            "ml-2 bg-[#21003d] p-2 rounded-md border-2 border-[#9e65ff] w-60 h-18 flex items-center justify-center "
+            "ml-2 bg-[#080610] p-2 rounded-md border-2 border-[#aa80f2] w-60 h-18 flex items-center justify-center "
           }
         >
           {title}
         </h2>
         <Icon type={type} />
       </div>
-      <div className={"m-4 w-60 h-49 bg-[#220d23] flex flex-col justify-around rounded-sm"}>
-        <h2 className={"text-center mt-1 text-3xl"}> {displayType} </h2>
-        <h2 className={"text-center mt-1"}> {time} Min. </h2>
-        <h2 className={"text-center mt-1"}> {notes} </h2>
+      <div className={"m-4 w-60 h-49 bg-[#080610] flex flex-col justify-around rounded-md border-[#aa80f2] border-1"}>
+        <h2 className={"text-center mt-1 text-3xl font-bold"}> {displayType} </h2>
+        <h2 className={"text-center mt-1 font-semibold"}> {time} Min. </h2>
+        <h2 className={"text-center mt-1 font-medium"}> {notes} </h2>
         <h2 className={"text-center mt-1 bg-black w-50 self-center rounded-2xl"}> Click for more details </h2>
       </div>
     </div>
