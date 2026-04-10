eccc5bd902dd083d2c323946718b2c778d58add2
Jacob Thomas <jdthomas2000@gmail.com>
2026-04-10 10:10:12 -0700

    cont profile pic edit

--- client/src/features/profile/Components/AvatarSelection.jsx
+++ client/src/features/profile/Components/AvatarSelection.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 
-export default function Avatar() {
+export default function AvatarSelection() {
   const base_pic = ["/Avatar/male.png", "/Avatar/female.png"];
   const head_wear = [
     "/Avatar/beanie.png",
@@ -16,12 +16,40 @@ export default function Avatar() {
     "/Avatar/chain.png",
   ];
 
-  const [base, setBase] = useState(base_pic[0]);
+  const [base, setBase] = useState([base_pic[0]]);
+  const [head, setHead] = useState();
+  const [chosenGloves, setChosenGloves] = useState();
+  const [chosenMisc, setChosenMisc] = useState([]);
+
   return (
     <>
       <div>
         Base
-        <img></img>
+        {base_pic.map((pic, id) => (
+          <img key={id} src={pic} onClick={() => setBase(pic)}></img>
+        ))}
+      </div>
+      <div>
+        Head
+        {head_wear.map((pic, id) => (
+          <img key={id} src={pic} onClick={() => setHead(pic)}></img>
+        ))}
+      </div>
+      <div>
+        Gloves
+        {gloves.map((pic, id) => (
+          <img key={id} src={pic} onClick={() => setChosenGloves(pic)}></img>
+        ))}
+      </div>
+      <div>
+        Misc
+        {misc.map((pic, id) => (
+          <img
+            key={id}
+            src={pic}
+            onClick={() => setChosenMisc([...chosenMisc, pic])}
+          ></img>
+        ))}
       </div>
     </>
   );
