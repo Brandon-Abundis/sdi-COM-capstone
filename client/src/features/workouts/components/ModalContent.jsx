ac16e091d94c2af6c86d7eb2b633ac484bd97066
Tafari <tafarigriffith2016@gmail.com>
2026-04-15 11:07:00 -0600

    Limits Workouts to last 30; Styles Buttons a bit; Adds Removal

--- client/src/features/workouts/components/ModalContent.jsx
+++ client/src/features/workouts/components/ModalContent.jsx
@@ -50,6 +50,37 @@ export default function ModalContent({ cancel, info }) {
     );
   };
 
+  const pseudoDel = () => {
+    let ress = document.querySelector("#listen");
+    const imOld = "1800-01-01T00:00:00.001Z";
+    // what a sad day
+    // i can't set the date to 1800 jan 1 D: D: D:
+    // wait i have a better idea >:3
+    const rewriteDate = {
+      name: `${titleRef.current?.value} - ${imOld}`,
+      type: typeRef.current?.value,
+      time: -1,
+      distance: -1,
+      reps: -1,
+      muscle_group: "Removed",
+      weight: -1,
+      notes: "Removed",
+      user_id: Number(id),
+    };
+    fetch(`http://localhost:8080/users/user_workouts/update/id/${storedId}`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(rewriteDate),
+    })
+      .then((data) => {
+        console.log(`status: ${data.status}`);
+        return data.json();
+      })
+      .then((data) => console.log(data));
+
+    progress(ress);
+  };
+
   let {
     title,
     type,
@@ -60,7 +91,7 @@ export default function ModalContent({ cancel, info }) {
     muscle_group,
     weight,
     storedId,
-    created_at,
+    updated_at,
   } = info
     ? info
     : {
@@ -72,7 +103,9 @@ export default function ModalContent({ cancel, info }) {
         notes: "",
         weight: "",
       };
-
+  let fiveDayAgo = new Date();
+  fiveDayAgo.setDate(fiveDayAgo.getDate() - 5);
+  const tooOld = updated_at < fiveDayAgo;
   let titleRef = useRef();
   let typeRef = useRef();
   let timeRef = useRef();
@@ -100,7 +133,6 @@ export default function ModalContent({ cancel, info }) {
       notes: noteRef.current?.value || "N/A",
       user_id: Number(id),
     };
-    console.log(payload);
     if (
       numberCheck(payload.time) ||
       numberCheck(payload.distance) ||
@@ -154,13 +186,17 @@ export default function ModalContent({ cancel, info }) {
     <div className="bg-transparent w-3xl h-200 flex flex-col rounded-lg items-center">
       <div
         id="inputList"
-        className="border-2 border-base-300 rounded-lg gap-2 bg-base-200 w-160 h-240 flex flex-col items-center justify-center text-secondary font-bold"
+        className="border-2 border-base-300 rounded-lg gap-2 bg-base-200 w-160 h-260 flex flex-col items-center justify-center text-secondary font-bold"
       >
         <h2
           id="listen"
           className="text-3xl font-bold text-primary mb-5 tracking-wide"
         >
-          {title != "" ? "Edit Workout" : "Create Workout"}
+          {storedId && tooOld
+            ? "View Workout"
+            : storedId
+              ? "Edit Workout"
+              : "Create Workout"}
         </h2>
         {storedId ? (
           <h3 className="text-base-content/50 text-xs">
@@ -171,17 +207,23 @@ export default function ModalContent({ cancel, info }) {
           <h3 className="text-base-content/30 text-xs">— New Workout —</h3>
         )}
 
-        {created_at ? (
+        {updated_at ? (
           <h3 className="text-base-content/50 text-xs">
             {" "}
-            Time Created: {created_at.toDateString()}{" "}
+            Time Created: {updated_at.toDateString()}{" "}
           </h3>
         ) : (
           <h3 className="text-base-content/30 text-xs">— No Date Yet —</h3>
         )}
 
         <h2> Title</h2>
-        <InputField ref={titleRef} style={"Title"} def={title} />
+        <InputField
+          ref={titleRef}
+          style={"Title"}
+          def={title}
+          aged={tooOld}
+          maxLen={40}
+        />
         <InputField
           ref={typeRef}
           style={"selection"}
@@ -196,19 +238,50 @@ export default function ModalContent({ cancel, info }) {
           chosen={type}
         />
         <h2> Time Estimate (Min.) - # </h2>
-        <InputField ref={timeRef} style={"Time Estimate (Min.)"} def={time} />
+        <InputField
+          ref={timeRef}
+          style={"Time Estimate (Min.)"}
+          def={time}
+          aged={tooOld}
+          maxLen={7}
+        />
         <h2> Distance (Mi.) - # </h2>
-        <InputField ref={distRef} style={"Distance (Mi.)"} def={distance} />
+        <InputField
+          ref={distRef}
+          style={"Distance (Mi.)"}
+          def={distance}
+          aged={tooOld}
+        />
         <h2> Reps - # </h2>
-        <InputField ref={repRef} style={"Reps"} def={reps} />
+        <InputField
+          ref={repRef}
+          style={"Reps"}
+          def={reps}
+          aged={tooOld}
+          maxLen={9}
+        />
         <h2> Muscle Group </h2>
-        <InputField ref={muscRef} style={"Muscle Group"} def={muscle_group} />
+        <InputField
+          ref={muscRef}
+          style={"Muscle Group"}
+          def={muscle_group}
+          aged={tooOld}
+        />
         <h2> Weight (lbs.) - # </h2>
-        <InputField ref={weightRef} style={"Weight (lbs.)"} def={weight} />
+        <InputField
+          ref={weightRef}
+          style={"Weight (lbs.)"}
+          def={weight}
+          aged={tooOld}
+          maxLen={9}
+        />
         <h2> Notes </h2>
-        <InputField ref={noteRef} style={"Notes"} def={notes} />
-        <div className="w-35 flex justify-between">
-          <Button name={"Submit"} func={() => submit()} />
+        <InputField ref={noteRef} style={"Notes"} def={notes} aged={tooOld} />
+        <div className="w-35 flex justify-center gap-2">
+          {!tooOld && <Button name={"Submit"} func={() => submit()} />}
+          {storedId && !tooOld && (
+            <Button name={"Remove"} func={() => pseudoDel()} />
+          )}
           <Button name={"Close"} func={() => cancel()} />
         </div>
       </div>
